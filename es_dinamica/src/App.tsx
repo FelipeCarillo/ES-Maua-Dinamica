698b44e57b6ca28eacf1040c619a813c220bcc77
Heistoo <46348338+Heistoo@users.noreply.github.com>
2024-08-27 12:28:56 -0300

    add text screens, algorithm & sort

--- es_dinamica/src/App.tsx
+++ es_dinamica/src/App.tsx
@@ -1,16 +1,32 @@
 import './App.css';
 import React from 'react';
-import logo from './logo.svg';
 
 function App() {
 
   const len_numbers = 8;
   const [numbers, setNumbers] = React.useState<number[]>([]);
 
+  const [targetIndex, setTargetIndex] = React.useState<string | undefined>();
+  const [targetValue, setTargetValue] = React.useState<number>();
+
   const handleSort = () => {
     let new_numbers = [...numbers];
     new_numbers.sort((a, b) => a - b);
     setNumbers(new_numbers);
+  }
+
+  const handleTarget = (event: any) => {
+    event.preventDefault();
+
+    if (targetIndex === undefined) {
+      return;
+    }
+
+    const target: number = Number(targetIndex);
+
+    console.log(numbers);
+
+    setTargetValue(numbers[target]);
   }
 
   const renderNumbersInput = () => {
@@ -34,6 +50,16 @@ function App() {
     return inputs;
   }
 
+  const renderSortedNumbers = () => {
+    let spans = [];
+    for (let i = 0; i < len_numbers; i++) {
+      spans.push(
+        <span> {numbers[i]} </span>
+      );
+    }
+    return spans;
+  }
+
   return (
     <div className="App" style={{maxWidth: "100%", overflow: "hidden"}}>
       <h1>Números</h1>
@@ -43,9 +69,32 @@ function App() {
       <button onClick={() => handleSort()} style={{marginTop: "10px"}}>Entrar</button>
       <hr/>
       <h1>
-        Busque Número
+        Números Ordenados
       </h1>
-      
+      <div style={{display: "flex", justifyContent:"center", alignItems:"center", gap:5 }}>
+        {renderSortedNumbers()}
+      </div>
+      <hr/>
+      <h1>
+        Busque Número pelo Index
+      </h1>
+      <form onSubmit={(event) => handleTarget(event)}>
+        <div style={{ display:"flex", justifyContent:"center", alignItems:"center", gap:50}}>
+          <div style={{ display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", gap:5}}>
+            <label>Insira o Index desejado</label>
+            <input onChange={(event) => setTargetIndex(event.target.value)}/>
+            <button type='submit'>Entrar</button>
+          </div>  
+          <div>
+            <label>Número:</label>
+            <div>
+              <span>
+                {targetValue}
+              </span>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
